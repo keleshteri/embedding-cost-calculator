dda9c0ebffc3567492a6c0dd64bbd2355ea922ae
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@outlook.com>
2025-05-06 15:02:48 +0800

    Add App.js file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmbeddingCostCalculator from './components/EmbeddingCostCalculator';
+
+function App() {
+  return (
+    <div className="App py-8">
+      <EmbeddingCostCalculator />
+    </div>
+  );
+}
+
+export default App;
